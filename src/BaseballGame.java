aebb608f9c917766246e5505b7b33c9bb57f3dd1
coldrice99 <coldrice99@gmail.com>
2024-09-13 11:29:54 +0900

    refactor: Lv2 ì입력값 유효 검사완료

--- src/BaseballGame.java
+++ src/BaseballGame.java
@@ -18,24 +18,27 @@ public class BaseballGame {
         while (true) {
             //유저에게 입력값을 받음
             System.out.println("숫자 3개를 입력하세요.");
-            Set<Integer> numberSet = new HashSet<>(); //입력값 저장을 위한 로컬 변수
+            List<Integer> inputList = new ArrayList<>(); //입력값 저장을 위한 로컬 변수. 순서를 위해 Set이 아닌 List
 
             boolean isValid = false; // 올바른 입력을 받을 때까지 반복
             while (!isValid) {
-                int input = sc.nextInt();
-                switch (validateInput(input, numberSet)) {
-                    case "중복":
-                        System.out.println("중복된 숫자는 입력할 수 없습니다.");
-                        break;
-                    case "세자리x":
-                        System.out.println("세 자리 숫자만 입력 가능합니다.");
-                        break;
-                    case "숫자x":
-                        System.out.println("숫자만 입력 가능합니다.");
-                        break;
-                    case "정상":
-                        isValid = true;
-                        break;
+                try {
+                    int input = sc.nextInt();
+                    switch (validateInput(input, inputList)) {
+                        case "중복":
+                            System.out.println("중복된 숫자는 입력할 수 없습니다.");
+                            break;
+                        case "세자리x":
+                            System.out.println("세 자리 숫자만 입력 가능합니다.");
+                            break;
+                        case "정상":
+                            isValid = true;
+                            break;
+                    }
+                } catch (InputMismatchException e) {
+                    //숫자가 아닌 값이 입력된 경우 처리
+                    System.out.println("숫자만 입력 가능합니다.");
+                    sc.next(); //입력 버퍼 초기화
                 }
             }
 
@@ -44,8 +47,8 @@ public class BaseballGame {
             System.out.println("게임 진행 횟수: " + gameCount);
 
             //스트라이크 && 볼 로직 처리
-            long strikes = countStrikeAndBall(numberSet,true);
-            long balls = countStrikeAndBall(numberSet,false);
+            long strikes = countStrikeAndBall(inputList,true);
+            long balls = countStrikeAndBall(inputList,false);
 
             //로직 처리
             if (strikes == 3) {
@@ -78,27 +81,25 @@ public class BaseballGame {
         return answerSet;
     }
 
-    protected String validateInput(int input, Set<Integer> numberSet) {
-        numberSet.clear(); // 기존 입력값 초기화 (재활용)
-
+    protected String validateInput(int input, List<Integer> inputList) {
+        inputList.clear(); // 기존 입력값 초기화 (재활용)
         if (input < 100 || input > 999) {
             return "세자리x"; // 세 자리 숫자가 아닌 경우
         }
 
-        numberSet.add(input / 100); // 백의 자리
-        numberSet.add((input / 10) % 10); // 십의 자리
-        numberSet.add(input % 10); // 일의 자리
+        inputList.add(input / 100); // 백의 자리
+        inputList.add((input / 10) % 10); // 십의 자리
+        inputList.add(input % 10); // 일의 자리
 
-        if (numberSet.size() < 3) {
+        if (new HashSet<>(inputList).size() < 3) {
             //같은 숫자가 입력되어 HashSet에 3자리 숫자가 저장되지 못하는 경우
             return "중복";
         }
         return "정상";
     }
 
-    private long countStrikeAndBall(Set<Integer> numberSet, boolean StrikeOrBall) {
-        List<Integer> answerList = new ArrayList<>(answerSet);
-        List<Integer> inputList = new ArrayList<>(numberSet);
+    private long countStrikeAndBall(List<Integer> inputList, boolean StrikeOrBall) {
+        List<Integer> answerList = new ArrayList<>(answerSet); //순서가 필요함으로 List를 사용
 
         //스트림에서 count() 메서드가 반환하는 값이 long 타입
         long strikes = 0;
